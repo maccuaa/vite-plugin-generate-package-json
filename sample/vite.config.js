7e922bd254fa0d826050320bac6a3568041e755e
Andrew MacCuaig <andrewmaccuaig@gmail.com>
2024-11-29 10:12:23 -0500

    vite 6 upgrade

--- sample/vite.config.js
+++ sample/vite.config.js
@@ -1,6 +1,6 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
-import { generatePackageJson } from "../dist";
+import { generatePackageJson } from "vite-plugin-generate-package-json";
 
 // https://vite.dev/config/
 export default defineConfig({
